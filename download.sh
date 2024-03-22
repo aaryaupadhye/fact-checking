d11fbd71c1462bc3cc3502c9a6b050494e5ef644
aaryaupadhye <aarya.upadhye@gmail.com>
2024-03-22 13:41:43 +0100

    added download.sh

---
+++ download.sh
@@ -0,0 +1,8 @@
+cd datasets
+
+wget https://nlp.cs.unc.edu/data/hover/wiki_wo_links.db
+
+wget https://github.com/hover-nlp/hover/blob/main/data/hover/hover_train_release_v1.1.json
+wget https://github.com/hover-nlp/hover/blob/main/data/hover/hover_dev_release_v1.1.json
+
+cd ..
